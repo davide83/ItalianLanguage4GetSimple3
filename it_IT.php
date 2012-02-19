aa3bee2fe45b8022a538d675ee3f02d358814f08
davide83 <davide.letizia@gmail.com>
2012-02-19 23:26:52 +0100

    first commit

---
+++ it_IT.php
@@ -0,0 +1,486 @@
+<?php
+/**
+ * Italian Language File
+ *
+ * Date:				19 Feb 2012
+ * Revision:		Feb 2012
+ * Version:			GetSimple 3.0
+ * Traductors: 	Davide Letizia 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+
+
+$i18n = array(
+
+/* 
+ * For: install.php
+*/
+"PHPVER_ERROR"			=>	"<b>Impossibile continuare:</b> PHP 5.1.3 o maggiore è richiesto, tu hai ",
+"SIMPLEXML_ERROR"		=>	"<b>Impossibile continuare:</b> <em>SimpleXML</em> non è installato",
+"CURL_WARNING"			=>	"<b>Attenzione:</b> <em>cURL</em> Non Installato",
+"TZ_WARNING"				=>	"<b>Attenzone:</b> <em>date_default_timezone_set</em> manca",
+"WEBSITENAME_ERROR"	=>	"<b>Errore:</b> C'è stato un problema con il titolo del sito",
+"WEBSITEURL_ERROR"	=>	"<b>Errore:</b> C'è stato un problema con l'URL del vostro sito web",
+"USERNAME_ERROR"		=>	"<b>Errore:</b> Username non impostato",
+"EMAIL_ERROR"				=>	"<b>Errore:</b> C'è stato un problema col tuo indirizzo email",
+"CHMOD_ERROR"				=>	"<b>Impossibile continuare:</b> Impossibile scrivere il file di config. <em>CHMOD 777</em> le cartelle /data, /backups e le loro sotto-cartelle e riprova",
+"EMAIL_COMPLETE"		=>	"Impostazioni Completate",
+"EMAIL_USERNAME"		=>	"La tua username è",
+"EMAIL_PASSWORD"		=>	"La tua nuova password è",
+"EMAIL_LOGIN"				=>	"Accesso qui",
+"EMAIL_THANKYOU"		=>	"Grazie per usare",
+"NOTE_REGISTRATION"	=>	"Le informazioni di registrazione sono state inviate a",
+"NOTE_REGERROR"			=>	"<b>Errore:</b> There was a problem sending out the registration information via email. Please make note of the password below",
+"NOTE_USERNAME"			=>	"Il tuo username è",
+"NOTE_PASSWORD"			=>	"e le tua password è",
+"INSTALLATION"			=>	"Installazione",
+"LABEL_WEBSITE"			=>	"Nome Website",
+"LABEL_BASEURL"			=>	"Website Base URL",
+"LABEL_SUGGESTION"	=>	"Il nostro suggerimento è",
+"LABEL_USERNAME"		=>	"Username",
+"LABEL_EMAIL"				=>	"Indirizzo Email",
+"LABEL_INSTALL"			=>	"Installa Adesso!",
+"SELECT_LANGUAGE"		=>  "Seleziona la tua lingua",
+"CONTINUE_SETUP" 		=>  "Continua con le Impostazioni",
+"DOWNLOAD_LANG" 		=>  "Scarica Lingue",
+
+/* 
+ * For: pages.php
+*/
+"MENUITEM_SUBTITLE"	=>	"voce menu",
+"HOMEPAGE_SUBTITLE"	=>	"homepage",
+"PRIVATE_SUBTITLE"	=>	"privato",
+"EDITPAGE_TITLE"		=>	"Modifica Pagina",
+"VIEWPAGE_TITLE"		=>	"Visualizza Pagina",
+"DELETEPAGE_TITLE"	=>	"Elimina Pagina",
+"PAGE_MANAGEMENT"		=>	"Gestione Pagina",
+"TOGGLE_STATUS"			=>	"Stato Commutatore",
+"TOTAL_PAGES"				=>	"numero totale di pagine",
+"ALL_PAGES"					=>	"Tutte le Pagine",
+
+/* 
+ * For: edit.php
+*/
+"PAGE_NOTEXIST"			=>	"La pagina richiesta non esiste",
+"BTN_SAVEPAGE"			=>	"Salva Pagina",
+"BTN_SAVEUPDATES"		=>	"Salva Aggiornamenti",
+"DEFAULT_TEMPLATE"	=>	"Template Predefinito",
+"NONE"							=>	"Nessuno",
+"PAGE"							=>	"Pagina",
+"NEW_PAGE"					=>	"Nuova Pagina",
+"PAGE_EDIT_MODE"		=>	"Modifica Pagina",
+"CREATE_NEW_PAGE"		=>	"Crea Nuova Pagina",
+"VIEW"							=>	"<em>V</em>isualizza",
+"PAGE_OPTIONS"			=>	"Opzio<em>n</em>i Pagina",
+"SLUG_URL"					=>	"Slug/URL",
+"TAG_KEYWORDS"			=>	"Tags &amp; Keywords",
+"PARENT_PAGE"				=>	"Pagina Genitore",
+"TEMPLATE"					=>	"Template",
+"KEEP_PRIVATE"			=>	"Mantenere la Pagina Privata?",
+"ADD_TO_MENU"				=>	"Aggiungere al Menu?",
+"PRIORITY"					=>	"Priorità",
+"MENU_TEXT"					=>	"Testo Menu",
+"LABEL_PAGEBODY"		=>	"Corpo Pagina",
+"CANCEL"						=>	"Cancella",
+"BACKUP_AVAILABLE"	=>	"Backup Disponibile",
+"MAX_FILE_SIZE"			=>	"Dimensione Massima file",
+"LAST_SAVED"				=>	"Ultimo Salvataggio",
+"FILE_UPLOAD"				=>	"Carica File",
+"OR"								=>	"oppure",
+
+/* 
+ * For: upload.php
+*/
+"ERROR_UPLOAD"			=>	"C'è stato un problema con caricamento del file",
+"FILE_SUCCESS_MSG"	=>	"Successo! Posizione File",
+"FILE_MANAGEMENT"		=>	"Gestione File",
+"UPLOADED_FILES"		=>	"File Caricati",
+"SHOW_ALL"					=>	"Mostra Tutto",
+"VIEW_FILE"					=>	"Visualizza File",
+"DELETE_FILE"				=>	"Elimina File",
+"TOTAL_FILES"				=>	"numero totale di file",
+
+/* 
+ * For: logout.php
+*/
+"MSG_LOGGEDOUT"			=>	"Ora sei disconnesso.",
+
+/* 
+ * For: index.php
+*/
+"LOGIN"							=>	"Accedi",
+"USERNAME"					=>	"Username",
+"PASSWORD"					=>	"Password",
+"FORGOT_PWD"				=>	"Hai dimenticato la password?",
+"CONTROL_PANEL"			=>	"Pannello di Controllo Accesso",
+
+/* 
+ * For: navigation.php
+*/
+"CURRENT_MENU" 			=> 	"Menu Corrente",
+"NO_MENU_PAGES" 		=> 	"Nessuna pagina è impostata ad apparire all'interno del menu principale",
+
+/* 
+ * For: theme-edit.php
+*/
+"TEMPLATE_FILE" 		=> 	"Il file Template <b>%s</b> è stato aggiornato con successo!",
+"THEME_MANAGEMENT" 	=> 	"Gestione Tema",
+"EDIT_THEME" 				=> 	"Modifica Tema",
+"EDITING_FILE" 			=> 	"Modificare File",
+"BTN_SAVECHANGES" 	=> 	"Salva Cambiamenti",
+"EDIT" 							=> 	"Modifica",
+
+/* 
+ * For: support.php
+*/
+"SETTINGS_UPDATED"	=> 	"Le impostazioni sono state aggiornate",
+"UNDO" 							=> 	"Annulla",
+"SUPPORT" 					=> 	"Supporto",
+"SETTINGS" 					=> 	"Impostazioni",
+"ERROR" 						=> 	"Errore",
+"BTN_SAVESETTINGS" 	=> 	"Salva Impostazioni",
+"VIEW_FAILED_LOGIN"	=> 	"Visualizza i Tentativi di Accesso Falliti",
+
+
+/* 
+ * For: log.php
+*/
+"MSG_HAS_BEEN_CLR" 	=> 	" è stato ripulito",
+"LOGS" 							=> 	"Logs",
+"VIEWING" 					=> 	"Visualizzazione",
+"LOG_FILE" 					=> 	"File di Log",
+"CLEAR_ALL_DATA" 		=> 	"Cancella tutti i dati da",
+"CLEAR_THIS_LOG" 		=> 	"<em>C</em>ancella Questo Log",
+"LOG_FILE_ENTRY" 		=> 	"VOCE LOG FILE",
+"THIS_COMPUTER"			=>	"Questo Computer",
+
+/* 
+ * For: backup-edit.php
+*/
+"BAK_MANAGEMENT"		=>	"Gestione Backup",
+"ASK_CANCEL"				=>	"<em>C</em>ancella", // 'c' is the accesskey identifier
+"ASK_RESTORE"				=>	"<em>R</em>ipristina", // 'r' is the accesskey identifier
+"ASK_DELETE"				=>	"<em>D</em>elimina", // 'd' is the accesskey identifier
+"BACKUP_OF"					=>	"Backup di",
+"PAGE_TITLE"				=>	"Titolo Pagina",
+"YES"								=>	"Si",
+"NO"								=>	"No",
+"DATE"							=>	"Data",
+"PERMS"							=>  "Permanente",
+
+/* 
+ * For: components.php
+*/
+"COMPONENTS"				=>	"Componenti",
+"DELETE_COMPONENT"	=>	"Elimina Componente",
+"EDIT"							=>	"Modifica",
+"ADD_COMPONENT"			=>	"<em>A</em>ggiungi Componente", // 'a' is the accesskey identifier
+"SAVE_COMPONENTS"		=>	"Salva Componenti",
+
+/* 
+ * For: sitemap.php
+*/
+"SITEMAP_CREATED"		=>	"Sitemap Creata! Con successo abbiamo pingato 4 motori di ricerca d'aggiornamento",
+"SITEMAP_ERRORPING"	=>	"Sitemap Creata, tuttavia si è verificato un errore pingando uno o più motori di ricerca",
+"SITEMAP_ERROR"			=>	"La sitemap non può essere generata",
+"SITEMAP_WAIT"			=>	"<b>Attendere Prego:</b> Creazione sitemap Sito",
+
+/* 
+ * For: theme.php
+*/
+"THEME_CHANGED"			=>	"Your theme has been changed successfully",
+"CHOOSE_THEME"			=>	"Scegli il Tuo Tema",
+"ACTIVATE_THEME"		=>	"Attiva Tema",
+"THEME_SCREENSHOT"	=>	"Schermata Tema",
+"THEME_PATH"				=>	"Percorso Tema Corrente",
+
+/* 
+ * For: resetpassword.php
+*/
+"RESET_PASSWORD"		=>	"Resetta Password",
+"YOUR_NEW"					=>	"La tua Nuova",
+"PASSWORD_IS"				=>	" password è",
+"ATTEMPT"						=>	"Prova",
+"MSG_PLEASE_EMAIL"	=>	"Inserisci la username registrata su questo sistema, e una nuova password vi sarà inviata all'indirizzo registrato.",
+"SEND_NEW_PWD"			=>	"Invia Nuova Password",
+
+/* 
+ * For: settings.php
+*/
+"GENERAL_SETTINGS"	=>	"Impostazioni Generali",
+"WEBSITE_SETTINGS"	=>	"Impostazioni Website",
+"LOCAL_TIMEZONE"		=>	"Fuso Orario Locale",
+"LANGUAGE"					=>	"Lingua",
+"USE_FANCY_URLS"		=>	"Usa Fancy URLs - <b style=\"font-weight:100\">Richiede che il <code>mod_rewrite</code> sia abilitato </b>",
+"ENABLE_HTML_ED"		=>	"<b>Abilita l'editor HTML</b>",
+"WARN_EMAILINVALID"	=>	"ATTENZIONE: Questo indirizzo email non risulta valido!",
+"ONLY_NEW_PASSWORD"	=>	"Fornire una password qiu di seguito solo se volete cambiare quella corrente",
+"NEW_PASSWORD"			=>	"Nuova Password",
+"CONFIRM_PASSWORD"	=>	"Conferma Password",
+"PASSWORD_NO_MATCH"	=>	"Le password non coincidono",
+"PERMALINK" 				=> 	"Custom Permalink Structure",
+"MORE" 							=> 	"Per saperne di più...",
+"HELP" 							=> 	"aiuto",
+
+/* 
+ * For: health-check.php
+*/
+"WEB_HEALTH_CHECK"	=>	"Controlla Salute Sito",
+"VERSION"						=>	"Versione",
+"UPG_NEEDED"				=>	"Aggiornamento necessario",
+"CANNOT_CHECK"			=>	"Impossibile verificare. La tua versione è",
+"LATEST_VERSION"		=>	"Ultima versione installata",
+"SERVER_SETUP"			=>	"Configura Server",
+"OR_GREATER_REQ"		=>	"o maggiore è richiesto",
+"OK"								=>	"OK",
+"INSTALLED"					=>	"Installato",
+"NOT_INSTALLED"			=>	"Non Installato",
+"WARNING"						=>	"Attenzione",
+"DATA_FILE_CHECK"		=>	"Data File Integrity Check",
+"DIR_PERMISSIONS"		=>	"Permessi Cartella",
+"EXISTANCE"					=>	"%s Esistenza",
+"MISSING_FILE"			=>	"File mancante",
+"BAD_FILE"					=>	"File Corrotto",
+"NO_FILE"						=>	"Nessun file",
+"GOOD_D_FILE"				=>	"Good 'Deny' file",
+"GOOD_A_FILE"				=>	"Good 'Allow' file",
+"CANNOT_DEL_FILE"		=>	"Impossibile Eliminare File",
+"DOWNLOAD"					=>	"Scarica",
+"WRITABLE"					=>	"Scrivibile",
+"NOT_WRITABLE"			=>	"Non Scrivibile",
+
+/* 
+ * For: footer.php
+*/
+"POWERED_BY"				=>	"Realizzato da",
+
+/* 
+ * For: backups.php
+*/
+"PAGE_BACKUPS"			=>	"Pagina Backups",
+"ASK_DELETE_ALL"		=>	"<em>E</em>limina Tutto",
+"DELETE_ALL_BAK"		=>	"Eliminare tutti i backups?",
+"TOTAL_BACKUPS"			=>	"Totale Backups",
+
+/* 
+ * For: archive.php
+*/
+"SUCC_WEB_ARCHIVE"	=>	"Archivio WebSite Riusciti!",
+"SUCC_WEB_ARC_DEL"	=>	"Archivio WebSite eliminato con successo",
+"WEBSITE_ARCHIVES"	=>	"Website Archivi",
+"ARCHIVE_DELETED"		=>	"Archivio eliminato con successo",
+"CREATE_NEW_ARC"		=>	"Crea un Nuovo Archivio",
+"ASK_CREATE_ARC"		=>	"<em>C</em>rea Nuovo Archivio Ora",
+"CREATE_ARC_WAIT"		=>	"<b>Attendere Prego:</b> Creazione archivio sito...",
+"DOWNLOAD_ARCHIVES"	=>	"Scarica Archivio",
+"DELETE_ARCHIVE"		=>	"Elimina Archivio",
+"TOTAL_ARCHIVES"		=>	"Archivi Totali",
+
+/* 
+ * For: include-nav.php
+*/
+"WELCOME"						=>	"Benvenuto", // used as 'Welcome USERNAME!'
+"TAB_PAGES"					=>	"<em>P</em>agine",
+"TAB_FILES"					=>	"F<em>i</em>les",
+"TAB_THEME"					=>	"<em>T</em>emi",
+"TAB_BACKUPS"				=>	"<em>B</em>ackups",
+"PLUGINS_NAV" 			=>  "Plu<em>g</em>ins",
+"TAB_SETTINGS"			=>	"<em>I</em>mpostazioni",
+"TAB_SUPPORT"				=>	"Supp<em>o</em>rto",
+"TAB_LOGOUT"				=>	"<em>E</em>sci",
+
+/* 
+ * For: sidebar-files.php
+*/
+"BROWSE_COMPUTER"		=>	"Sfoglia sul Computer",
+"UPLOAD"						=>	"Carica",
+
+/* 
+ * For: sidebar-support.php
+*/
+"SIDE_SUPPORT_LOG"	=>	"Supp<em>o</em>rto",
+"SIDE_HEALTH_CHK"		=>	"Controlla la <em>S</em>alute del Sito",
+"SIDE_DOCUMENTATION"=>	"Documentazione",
+"SIDE_VIEW_LOG"     =>	"<em>V</em>isualizza Log",
+
+/* 
+ * For: sidebar-theme.php
+*/
+"SIDE_VIEW_SITEMAP"	=>	"<em>V</em>isualizza Sitemap",
+"SIDE_GEN_SITEMAP"	=>	"Genera Site<em>m</em>ap",
+"SIDE_COMPONENTS"		=>	"<em>M</em>odifica Componenti",
+"SIDE_EDIT_THEME"		=>	"Modifica T<em>e</em>ma",
+"SIDE_CHOOSE_THEME"	=>	"Scegli <em>T</em>ema",
+
+/* 
+ * For: sidebar-pages.php
+*/
+"SIDE_CREATE_NEW"		=>	"<em>C</em>rea Nuova Pagina",
+"SIDE_VIEW_PAGES"		=>	"Visualizza Tutte le <em>P</em>agine",
+
+/* 
+ * For: sidebar-settings.php
+*/
+"SIDE_GEN_SETTINGS"	=>	"<em>I</em>mpostazioni Generali",
+"SIDE_USER_PROFILE"	=>	"<em>U</em>ser Profilo",
+
+/* 
+ * For: sidebar-backups.php
+*/
+"SIDE_VIEW_BAK"			=>	"Visualizza Pagina Backup",
+"SIDE_WEB_ARCHIVES"	=>	"<em>W</em>ebsite Archivi",
+"SIDE_PAGE_BAK"			=>	"Pagina <em>B</em>ackups",
+
+/* 
+ * For: error_checking.php
+*/
+"ER_PWD_CHANGE"			=>	"Non dimenticare di <a href=\"settings.php#profile\">cambiare la password</a> da quella casualmente generata...",
+"ER_BAKUP_DELETED"	=>	"Il backup per %s è stato eliminato",
+"ER_REQ_PROC_FAIL"	=>	"Il processo richiesto non è riuscito",
+"ER_YOUR_CHANGES"		=>	"I cambiamenti a %s sono stati salvati",
+"ER_HASBEEN_REST"		=>	"%s è stato ripristinato",
+"ER_HASBEEN_DEL"		=>	"%s è stato eliminato",
+"ER_CANNOT_INDEX"		=>	"Non puoi cambiare la URL della Pagina iniziale",
+"ER_SETTINGS_UPD"		=>	"Le impostazioni sono state aggiornate",
+"ER_OLD_RESTORED"		=>	"Le vecchie impostazioni sono state ripristinate",
+"ER_NEW_PWD_SENT"		=>	"Una nuova password è stata inviata all'indirizzo di posta elettronica fornito",
+"ER_SENDMAIL_ERR"		=>	"C'è stato un problema con l'invio della email. Riprova",
+"ER_FILE_DEL_SUC"		=>	"File eliminato con successo",
+"ER_PROBLEM_DEL"		=>	"C'è stato un problema cancellando il file",
+"ER_COMPONENT_SAVE"	=>	"I componenti sono stati salvati",
+"ER_COMPONENT_REST"	=>	"I componenti sono stati ripristinati",
+"ER_CANCELLED_FAIL"	=>	"<b>Cancellato:</b> Questo aggiornamanto è stato cancellato",
+
+/* 
+ * For: changedata.php
+*/
+"CANNOT_SAVE_EMPTY"	=>	"Non puoi salvare una pagina vuota",
+"META_DESC" 				=> "Descrizione Meta",
+
+/* 
+ * For: template_functions.php
+*/
+"FTYPE_COMPRESSED"	=>	"Compressi", //a file-type
+"FTYPE_VECTOR"			=>	"Vettori", //a file-type
+"FTYPE_FLASH"				=>	"Flash", //a file-type
+"FTYPE_VIDEO"				=>	"Video", //a file-type
+"FTYPE_AUDIO"				=>	"Audio", //a file-type
+"FTYPE_WEB"					=>	"Web", //a file-type
+"FTYPE_DOCUMENTS"		=>	"Documenti", //a file-type
+"FTYPE_SYSTEM"			=>	"Sistema", //a file-type
+"FTYPE_MISC"				=>	"Misc", //a file-type
+"IMAGES"						=>	"Immagini",
+
+/* 
+ * For: login_functions.php
+*/
+"FILL_IN_REQ_FIELD"	=>	"Si prega di compilare tutti i campi richiesti",
+"LOGIN_FAILED"			=>	"Accesso fallito. Si prega di ricontrollare il tuo Username e la Password",
+
+/* 
+ * For: Date Format
+*/
+"DATE_FORMAT"									=>	"j M Y", //please keep short
+"DATE_AND_TIME_FORMAT"				=>	"l j F Y - G:i", //date and time
+
+/* 
+ * For: welcome.php
+*/
+"WELCOME_MSG"				=>	"Grazie per aver scelto GetSimple come tuo CMS!",
+"WELCOME_P"					=>	"GetSimple rende la gestione del tuo sito il più semplice possibile con la sua interfaccia utente e una facile gestione dei template.",
+"GETTING_STARTED"		=>	"Per Iniziare",
+
+/* 
+ * For: image.php
+*/
+
+"CURRENT_THUMBNAIL" => "Miniatura Corrente",
+"RECREATE" 					=> "ricrea",
+"CREATE_ONE" 				=> "creane uno",
+"IMG_CONTROl_PANEL" => "Immagine Pannello di Controllo",
+"ORIGINAL_IMG" 			=> "Immagine Originale",
+"CLIPBOARD_INSTR" 	=> "Seleziona Tutto",
+"CREATE_THUMBNAIL" 	=> "Crea Miniatura",
+"CROP_INSTR_NEW" 		=> "<em>ctrl-B</em> o <em>command-B</em> per squadrare",
+"SELECT_DIMENTIONS" => "Seleziona Dimensioni",
+"HTML_ORIG_IMG" 		=> "Immagine Originale HTML",
+"LINK_ORIG_IMG" 		=> "Immagine Originale Link",
+"HTML_THUMBNAIL" 		=> "Miniatura HTML",
+"LINK_THUMBNAIL" 		=> "Miniatura Link",
+"HTML_THUMB_ORIG" 	=> "Miniatura-a-Immagine HTML",
+
+/* 
+ * For: plugins.php
+*/
+
+"PLUGINS_MANAGEMENT"=> "Gestione Plugin",
+"PLUGINS_INSTALLED" => "plugins installati",
+"SHOW_PLUGINS"			=> "Plu<em>g</em>ins Instalati",
+"PLUGIN_NAME" 			=> "Nome",
+"PLUGIN_DESC" 			=> "Descrizione",
+"PLUGIN_VER" 				=> "Versione",
+
+
+
+
+/***********************************************************************************
+ * SINCE Version 3.0
+***********************************************************************************/
+
+/* 
+ * For: setup.php
+ */
+
+"ROOT_HTACCESS_ERROR" => "La creazione di .htaccess nella root è fallita! Si prega di copiare <b>%s</b> in <b>.htaccess</b> e cambiare <code>%s</code> con <code>%s</code>",
+"REMOVE_TEMPCONFIG_ERROR" => "Impossibile eliminare <b>%s</b>! Si prega di farlo manualmente.",
+"MOVE_TEMPCONFIG_ERROR" => "Impossibile rinominare <b>%s</b> in <b>%s</b>! Si prega di farlo manualmente.",
+"KILL_CANT_CONTINUE" => "Impossibile continuare. Si prega di correggere gli errori e riprovare.",
+"REFRESH" => "Ricarica",
+"BETA"=> "Beta / Bleeding Edge",
+
+/*
+ * Misc Cleanup Work
+ */
+ 
+"HOMEPAGE_DELETE_ERROR" => "Non puoi cancellare la pagina Home", //deletefile
+"NO_ZIPARCHIVE" => "L'estensione ZipArchive non è installata. Impossibile continuare", //zip
+"REDIRECT_MSG"=> "Se il tuo browser non ti reindirizza, clicca <a href=\"%s\">qui</a>", //basic
+"REDIRECT"=> "Reindirizza", //basic
+"DENIED"=> "Negato", //sitemap
+"DEBUG_MODE"=> "MODALITA' DEBUG", //nav-include
+"DOUBLE_CLICK_EDIT"=> "Doppio Click per Modificare", //components
+"THUMB_SAVED"=> "Miniatura Salvata", //image
+"EDIT_COMPONENTS"		=>	"Modifica Componenti", //components
+"REQS_MORE_INFO"=> "Per maggiori informazioni sui requisiti dei moduli, visita la <a href=\"%s\" target=\"_blank\" >pagina dei requisiti</a>.", //install & health-check
+"SYSTEM_UPDATE" => "Aggiorna Sistema", // update.php
+"AUTHOR" 				=> "Autore", //plugins.php
+"ENABLE" 				=> "Abilita", //plugins.php
+"DISABLE" 				=> "Disabilita", //plugins.php
+"NO_THEME_SCREENSHOT" => "Nessun Tema disponbile", //theme.php
+"UNSAVED_INFORMATION" => "Stai per uscire da questa pagina perdendo qualsiasi informazione non salvata.", //edit.php
+"BACK_TO_WEBSITE" => "Torna al Sito", //index & resetpassword
+"SUPPORT_FORUM" => "Forum di Supporto", //support.php
+"FILTER" => "Filtra", //pages.php
+"UPLOADIFY_BUTTON" => "Carica files e/o immagini...", //upload.php
+"FILE_BROWSER" => "Visualizza File", //filebrowser.php
+"SELECT_FILE" => "Seleziona file", //filebrowser.php
+"CREATE_FOLDER" => "Crea Cartella", //upload.php
+"THUMBNAIL" => "Miniatura", //filebrowser.php
+"ERROR_FOLDER_EXISTS" => "La cartella che hai provato a creare esiste già", //upload.php
+"FOLDER_CREATED" => "La nuova cartella è stata creata con successo: <strong>%s</strong>", //upload.php
+"ERROR_CREATING_FOLDER" => "C'è stato un errore nella creazione della nuova cartella", //upload.php
+"DELETE_FOLDER" => "Elimina Cartella", //upload.php
+"FILE_NAME" => "Nome File", //multiple tr header rows
+"FILE_SIZE" => "Dimensione", //multiple tr header rows
+"ARCHIVE_DATE" => "Data Archivio", //archive.php
+"CKEDITOR_LANG" => "en", // edit.php ; set CKEditor language, don't forget to include CKEditor language file in translation zip
+
+"X" => "non tradotte"
+
+
+
+);
+
+?>
